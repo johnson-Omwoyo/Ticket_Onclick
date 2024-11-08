95b701160d5727e31160981be70ee951c9ce8e59
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-11-08 13:50:42 +0300

    Made some home designs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Footer from "./Components/Footer";
 
 function App() {
