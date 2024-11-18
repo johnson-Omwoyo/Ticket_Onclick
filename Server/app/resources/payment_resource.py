2ec029ffc21d2c221ca025a1002730d31c0f5952
iyiola olusola ebenezer <sholaiyiola1966@gmail.com>
2024-11-18 10:23:08 +0300

    new payment

---
+++ Server/app/resources/payment_resource.py
@@ -0,0 +1,34 @@
+from flask import Blueprint
+from flask_restful import Api, reqparse, Resource
+from ..models import Payment, db
+
+bp = Blueprint("payment", _name_)
+api = Api(bp)
+
+
+class PaymentResource(Resource):
+    fields = ["event_id", "user_id", "amount", "date", "time"]
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        for field in PaymentResource.fields:
+            parser.add_argument(field, required=True, help=f"{field} cannot be empty")
+        data = parser.parse_args()
+
+        new_payment = Payment(
+            event_id=data.get("event_id"),
+            user_id=data.get("user_id"),
+            amount=data.get("amount"),
+            date=data.get("date"),
+            time=data.get("time"),
+        )
+
+        db.session.add(new_payment)
+        db.session.commit()
+        return {"message": "Payment added successfully"}, 201
+
+    def get(self, payment_id=None):
+        if payment_id is not None:
+            payment = Payment.query.get_or_404(payment_id)
+            return payment.to_dict()
+        return [payment.to_dict() for payment in Payment.query.all()], 200
